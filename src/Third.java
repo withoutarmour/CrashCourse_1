edc0d2ab0f527fd5e908f95d8a1ceec14e520c86
Sviatoslav <Slawik36577@gmail.com>
2020-10-23 14:05:47 +0300

    a b v g tasks

---
+++ src/Third.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Third {
+    public static void main(System[] args)throws IOException {
+        System.out.println("Введіть радіус кола:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int r = Integer.parseInt(br.readLine());
+        int r2 = r*r;
+        float s = (float) (r2 * 3.14);
+        float p = (float) (2*3.14*r);
+        System.out.println("Периметр: "+p+" Площа: "+s);
+
+
+    }
+}
