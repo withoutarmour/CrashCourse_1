ee6213d02deae59fc11b69d8ed7aeac66e5cfe3f
Sviatoslav <Slawik36577@gmail.com>
2020-10-23 14:13:52 +0300

    a b v g tasks

--- src/Third.java
+++ src/Third.java
@@ -4,13 +4,20 @@ import java.io.InputStreamReader;
 
 public class Third {
     public static void main(String[] args)throws IOException {
-        System.out.println("Введіть радіус кола:");
+      //  System.out.println("Введіть радіус кола:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int r = Integer.parseInt(br.readLine());
+      /*  int r = Integer.parseInt(br.readLine());
         int r2 = r*r;
         float s = (float) (r2 * 3.14);
         float p = (float) (2*3.14*r);
-        System.out.println("Периметр: "+p+" Площа: "+s);
+        System.out.println("Периметр: "+p+" Площа: "+s);*/
+        String name;
+        String address;
+        System.out.println("What is your name?");
+        name = br.readLine();
+        System.out.println("What do you live, "+name+"?");
+        address = br.readLine();
+        System.out.println("Name: "+name+" address: "+address);
 
 
     }
