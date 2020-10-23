ed521ec8ff4b78f790008e507be428828b3a7f24
Sviatoslav <Slawik36577@gmail.com>
2020-10-23 14:25:53 +0300

    a b v g tasks

--- src/Third.java
+++ src/Third.java
@@ -6,18 +6,39 @@ public class Third {
     public static void main(String[] args)throws IOException {
       //  System.out.println("Введіть радіус кола:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /** 1) **/
       /*  int r = Integer.parseInt(br.readLine());
         int r2 = r*r;
         float s = (float) (r2 * 3.14);
         float p = (float) (2*3.14*r);
         System.out.println("Периметр: "+p+" Площа: "+s);*/
-        String name;
+        /** 2) **/
+        /* String name;
         String address;
         System.out.println("What is your name?");
         name = br.readLine();
         System.out.println("What do you live, "+name+"?");
         address = br.readLine();
         System.out.println("Name: "+name+" address: "+address);
+*/
+        /** 3) **/
+       /* int c1,c2,c3;
+        int t1,t2,t3;
+        System.out.println("Введіть комісію за хвилину 1, 2 та 3 країн:");
+        c1 = Integer.parseInt(br.readLine());
+        c2 = Integer.parseInt(br.readLine());
+        c3 = Integer.parseInt(br.readLine());
+        System.out.println("Введіть час розмови 1, 2 та 3 людини:");
+        t1 = Integer.parseInt(br.readLine());
+        t2 = Integer.parseInt(br.readLine());
+        t3 = Integer.parseInt(br.readLine());
+        int sep1 = c1*t1;
+        int sep2 = c2*t2;
+        int sep3 = c3*t3;
+        int tog = sep1+sep2+sep3;
+        System.out.println("Окремо кожен 1-3: "+sep1+", "+sep2+", "+sep3);
+        System.out.println("Разом: "+tog);*/
+
 
 
     }
